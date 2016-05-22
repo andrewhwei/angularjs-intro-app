a9cada6e13c4bc85d749091d56fdbfd9eb5297b3
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-22 12:00:24 -0700

    more angular functions

--- app/assets/javascripts/examplCtrl.js
+++ app/assets/javascripts/examplCtrl.js
@@ -2,5 +2,10 @@
   "use strict";
   angular.module("app").controller("exampleCtrl", function($scope) {
     $scope.tasks = ["Walk the dog", "Cook dinner", "Vacuum the house"];
+
+    $scope.alertMe = function(message) {
+      alert(message);
+    }
+
   });
 }());
