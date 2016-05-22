a6e33307c851a427498f8a5568f7381b59d527e2
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-22 11:45:27 -0700

    refactored task list code

--- app/assets/javascripts/examplCtrl.js
+++ app/assets/javascripts/examplCtrl.js
@@ -1,8 +1,6 @@
 (function() {
   "use strict";
   angular.module("app").controller("exampleCtrl", function($scope) {
-    $scope.task1 = "Walk the dog";
-    $scope.task2 = "Cook dinner";
-    $scope.task3 = "Vacuum the house";
+    $scope.tasks = ["Walk the dog", "Cook dinner", "Vacuum the house"];
   });
 }());
